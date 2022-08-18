8553a18f05c230389264ddeb7a35732c66ce7f4b
Lais <sanolilais@gmail.com>
2022-08-18 09:04:44 -0300

    fix - movie search now displays only one movie list at a time, without clearing previous movie search

--- script/script.js
+++ script/script.js
@@ -1,54 +1,37 @@
-const btn = document.getElementById('btncheck')
-
-btn.addEventListener('change' , () => {
-  document.body.classList.toggle('dark')
-})
-                      
-
 function createCards (data) {
-data.Search.forEach(movie => {
-const containerMovies = document.getElementById("containerCard")
- containerMovies.insertAdjacentHTML('beforeend', 
+ const containerMovies = document.getElementById("containerCard")
+ containerMovies.innerHTML = ''
+  data.Search.forEach(movie => {
+   containerMovies.insertAdjacentHTML('beforeend', 
 `
   <div class="card">
     <img src=${movie.Poster} id="posters" alt="">
     <div class="info-movie texts-about-movie">
     <div class="movie-name" id="moviename">
-    ${movie.Title}
+     ${movie.Title}
     </div>
     <span id="premieredate">
-    ${movie.Year}
+     ${movie.Year}
     </span>
+    <button type="button" class="btn-showmore" id="btnshowMore">Ver Mais</button>
     </div>      
     </div>
 ` )
-}); 
+ }); 
 }
-
 async function getMovies(movies) {
-  const responseMovies = (`http://omdbapi.com/?s=${movies}&apikey=f3d97dd1`)
-
-  const response = await fetch (responseMovies).then(response => response.json().then(res => res))
-  
-  console.log(response)
-createCards(response)    
+ const responseMovies = (`http://omdbapi.com/?s=${movies}&apikey=f3d97dd1`)
+ const response = await fetch (responseMovies).then(response => response.json()
+  .then(res => res))
+   createCards(response)  
 } 
-
-function searchMovie() {
- 
-
-listMovies($searchResults)
-}
-
-async function listMovies(searchResults) {
-  const findMovie = ('https://omdbapi.com/?s='+searchResults)
-  const res = await fetch (findMovie)
-  const datamovies = await res.json()
-}
-
-const $searchResults = document.getElementById("searchBox").value
+async function listMovies(searchResults) { 
+ const findMovie = ('https://omdbapi.com/?s='+searchResults)
+ const res = await fetch (findMovie)
+ const datamovies = await res.json()
+} 
 const $btnsearch = document.getElementById("btnsearch")
-$btnsearch.addEventListener('click' , () => {
-  const $searchResults = document.getElementById("searchBox").value
-  getMovies($searchResults)
+ $btnsearch.addEventListener('click' , () => { 
+ const $searchResults = document.getElementById("searchBox").value  
+ getMovies($searchResults)
 })
